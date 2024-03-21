ef32c0bf117acfd2a87b7db562db589671607695
GeraldoSiqueiraNeto <geraldonetosiqueira@gmail.com>
2024-03-21 09:58:31 -0300

    Integration tests - valid fields on update method

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -104,4 +104,27 @@ describe('UserEntity integration tests', () => {
       new UserEntity(props)
     })
   })
+
+  describe('Update method', () => {
+    it('Should throw an error when update a user with invalid name', () => {
+      const entity = new UserEntity(UserDataBuilder({}))
+
+      expect(() => entity.update(null)).toThrow(EntityValidationError)
+      expect(() => entity.update('')).toThrow(EntityValidationError)
+      expect(() => entity.update(10 as any)).toThrow(EntityValidationError)
+      expect(() => entity.update('a'.repeat(256))).toThrow(
+        EntityValidationError,
+      )
+    })
+
+    it('Should a valid user', () => {
+      expect.assertions(0)
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+      const entity = new UserEntity(props)
+      entity.update('new name')
+    })
+  })
 })
