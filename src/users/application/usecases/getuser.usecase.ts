0575174c23d1c3d1cb1a61b027ac51fe190e2e9b
GeraldoSiqueiraNeto <geraldonetosiqueira@gmail.com>
2024-03-28 15:40:20 -0300

    Refactoring usecases

--- src/users/application/usecases/getuser.usecase.ts
+++ src/users/application/usecases/getuser.usecase.ts
@@ -1,20 +1,12 @@
-import { HashProvider } from '../../../shared/application/providers/hash-provider'
-import { UserEntity } from '../../domain/entities/user.entity'
 import { UserRepository } from '../../domain/repositories/user.repository'
-import { BadRequestError } from '../errors/bad-request-error'
+import { UserOutput } from '../dto/user-output'
 
 export namespace GetUserUseCase {
   export type Input = {
     id: string
   }
 
-  export type Output = {
-    id: string
-    name: string
-    email: string
-    password: string
-    createdAt: Date
-  }
+  export type Output = UserOutput
 
   export class UseCase {
     constructor(private userRepository: UserRepository.Repository) {}
